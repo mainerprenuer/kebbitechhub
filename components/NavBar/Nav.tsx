996501b81a745ae8e414e557c69cdc6c8695ad4e
Mousty Mainer <moustymainer@gmail.com>
2024-12-26 19:00:19 +0100

    Update Nav.tsx

--- components/NavBar/Nav.tsx
+++ components/NavBar/Nav.tsx
@@ -1,27 +1,56 @@
+'use client';
+
 import { navLinks } from '@/constant/constant'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { HiBars3BottomRight } from 'react-icons/hi2'
 
 const Nav = () => {
-  return (
-    <div className='fixed w-full transition-all duration-200 h-[12vh] z-[1000] bg-green-700'>
-        <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto border-r">
-            {/* logo */}
-            <Image src="/images/logo copy.png" alt='Logo' width={170} height={170} />
-            {/* Nav Links */}
-            <div className='hidden lg:flex items-center space-x-10'>
-                {navLinks.map((link) => {
-                    return (
-                        <Link key={link.id} href={link.url}>
-                            <p className='nav__link'>{link.label}</p>
-                        </Link>
-                    )
-                })}
+    const [navBg, setNavBg] = useState(false);
+
+    useEffect(() => {
+        const handler = () => {
+            if (window.scrollY >= 90) {
+                setNavBg(true);
+            }
+            if (window.scrollY <= 90) {
+                setNavBg(false);
+            }
+        }
+
+        window.addEventListener("scroll", handler)
+
+        return () => window.removeEventListener("scroll", handler)
+    })
+
+    return (
+        <div className={`fixed ${navBg ? 'bg-green-400' : 'fixed'} w-full transition-all duration-200 h-[12vh] z-[1000] bg-green-700`}>
+            <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
+                {/* logo */}
+                <Image src="/images/logo copy.png" alt='Logo' width={140} height={140} className='rounded-lg' />
+                {/* Nav Links */}
+                <div className='hidden lg:flex items-center space-x-10'>
+                    {navLinks.map((link) => {
+                        return (
+                            <Link key={link.id} href={link.url}>
+                                <p className='nav__link'>{link.label}</p>
+                            </Link>
+                        );
+                    })}
+                </div>
+                {/* Buttons */}
+                <div className='flex items-center space-x-4'>
+                    <button className='md:px-10 md:py-2 px-8 py:1.5 text-white font-semibold text-base bg-red-700
+                    hover:bg-red-900 transition-all duration-200 rounded-lg'>
+                        Signup Now
+                    </button>
+                    {/* burger menu  */}
+                    <HiBars3BottomRight className='w-8 h-8 cursor-pointer text-white lg:hidden '/>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Nav
