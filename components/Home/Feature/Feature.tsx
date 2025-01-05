5c12fbea093c9afcb16c4c8da60c12abbf7db3d9
Mousty Mainer <moustymainer@gmail.com>
2025-01-05 20:43:27 +0100

    update static data

--- components/Home/Feature/Feature.tsx
+++ components/Home/Feature/Feature.tsx
@@ -12,7 +12,7 @@ const Feature = () => {
                 {/* image  */}
                 <Tilt>
                     <div data-aos="zoom-in" data-aos-anchor-placement="top-center">
-                        <Image src="/images/f.png" alt='FeatureImage' width={1000} height={1000} />
+                        <Image src="/images/d6.jpg" alt='FeatureImage' width={1000} height={1000} className='rounded-lg' />
                     </div>
                 </Tilt>
                 {/* text content  */}
@@ -20,14 +20,14 @@ const Feature = () => {
                         {/* subheading  */}
                         <div className="flex items-center space-x-4">
                             <div className='w-12 h-12 bg-rose-600 rounded-full flex items-center justify-center flex-col'>
-                                <FaBriefcase className='h-6 w-6 text-white' />
+                                <FaRobot className='h-6 w-6 text-white' />
                             </div>
-                            <h1 className='text-xl text-black font-semibold'>Premium Learning experience</h1>
+                            <h1 className='text-xl text-black font-semibold'>Build the Future of Robotics</h1>
                         </div>
                         <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-6xl mt-8 font-bold md:leading-[3rem] lg:leading-[3.5rem] xl:leading-[3.9rem] text-gray-800'>
                         Elevate Your Learning Journey.
                         </h1>
-                        <p className='mt-4 text-gray-600'>Experience the difference with a premium learning journey. Our expertly crafted courses, top-tier instructors, and unparalleled support provide an exceptional educational experience that empowers you to achieve your goals.</p>
+                        <p className='mt-4 text-gray-600'>Introduce young minds to the exciting world of robotics! Our program provides a playful and interactive learning experience that builds confidence and inspires innovation.</p>
                         <button className='flex items-center space-x-2 px-8 py-3 mt-8 hover:bg-gray-700 transition-all duration-200 rounded-3xl bg-black text-white'>
                             <span>Learn More</span>
                             <FaArrowRight />
